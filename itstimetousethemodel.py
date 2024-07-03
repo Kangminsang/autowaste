ac19ac8785d15def2b7436cef484e1e0e82b1e9a
Kangminsang <kangminsang1219@gmail.com>
2024-07-03 20:39:58 +0900

    모델 불러오기 추가

--- itstimetousethemodel.py
+++ itstimetousethemodel.py
@@ -5,10 +5,13 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-device = ("cuda"
+device = torch.device("cuda"
           if torch.cuda.is_available()
           else "cpu"
 )
 print(f"{device}를 리용합네다")
 
-model = resnet18()
+model = resnet18().to(device)
+model.load_state_dict(torch.load('./models/weights.pt', map_location=device))
+from torchsummary import summary
+summary(model, (3, 224, 224), device=device.type)
